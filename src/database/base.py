d20508c9b73f7ebdab63a0a4f121a3b2d0738fdd
ThiagoSenaJT <thiago.sena@justtraveltour.com>
2025-11-16 23:28:20 -0300

    add psycopg2

--- src/database/base.py
+++ src/database/base.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from src.database.config import settings
 import os
 from dotenv import load_dotenv
 
