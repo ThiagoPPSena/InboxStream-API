b0159ebaaf292b369522ce8c4c84e0537ab71217
ThiagoSenaJT <thiago.sena@justtraveltour.com>
2025-11-16 23:15:33 -0300

    fix database_url

--- src/database/base.py
+++ src/database/base.py
@@ -2,8 +2,12 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from src.database.config import settings
+import os
+from dotenv import load_dotenv
 
-engine = create_async_engine(settings.DATABASE_URL, echo=False)
+load_dotenv()
+
+engine = create_async_engine(os.getenv('DATABASE_URL'), echo=False)
 
 AsyncSessionLocal = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
